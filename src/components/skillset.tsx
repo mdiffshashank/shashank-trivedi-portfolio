3b77c29a817747ab95067bec140373c62d3d16c6
Shashank Trivedi <mdiffshashank@gmail.com>
2024-07-08 00:55:19 +0530

    more logos added

--- src/components/skillset.tsx
+++ src/components/skillset.tsx
@@ -7,9 +7,22 @@ const SkillSet = () => {
       <h1 className="capitalize mt-20 mb-20  text-4xl text-center">
         Skills achieved over the years
       </h1>
-      <div className="grid grid-cols-5 gap-2 mx-auto w-7/12 text-center ">
+      <div className="grid grid-cols-5 gap-8 mx-auto w-7/12 text-center ">
+        <Image
+          src={"/html_logo.svg"}
+          alt={"HTML Logo"}
+          width={50}
+          height={50}
+        />
+        <Image src={"/css-logo.svg"} alt={"CSS Logo"} width={50} height={50} />
         <Image src={"/js-logo.png"} alt={"JS Logo"} width={50} height={50} />
-
+        <Image
+          src={"/ts-logo.svg"}
+          alt={"TS Logo"}
+          width={50}
+          height={50}
+          className="border-2"
+        />
         <Image
           src={"/react-logo.svg"}
           alt={"React Logo"}
@@ -20,21 +33,6 @@ const SkillSet = () => {
         <Image
           src={"/next-js-logo.svg"}
           alt={"next Logo"}
-          width={50}
-          height={50}
-          className="border-2"
-        />
-
-        <Image
-          src={"/ts-logo.svg"}
-          alt={"TS Logo"}
-          width={50}
-          height={50}
-          className="border-2"
-        />
-        <Image
-          src={"/css-logo.svg"}
-          alt={"CSS Logo"}
           width={50}
           height={50}
           className="border-2"
