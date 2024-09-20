00510ac0f05e64e903e87b130b7767ac6533270e
Shashank Trivedi <mdiffshashank@gmail.com>
2024-09-20 19:51:18 +0530

    link corrected

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,10 +5,7 @@ const Projects = () => {
     <div className="p-8 mx-auto  mt-20 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
       <Card>
         <div className={"text-xl text-center"}>
-          <a
-            href="https://todo-gety9rw66-shashank-trivedis-projects-3a9462cb.vercel.app/"
-            target="_blank"
-          >
+          <a href="https://todo-app-dun-phi.vercel.app/" target="_blank">
             Todo- APP with Data Persistence
           </a>
         </div>
