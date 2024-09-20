4c79ce94eafdea24e6678cc460dfaf3014569c9e
Shashank Trivedi <mdiffshashank@gmail.com>
2024-09-20 12:16:12 +0530

    todo app added

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,28 +4,14 @@ const Projects = () => {
   return (
     <div className="p-8 mx-auto  mt-20 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
       <Card>
-        <div className={"text-xl text-center"}>Formik components</div>
-      </Card>
-      <Card>
-        <div className={"text-xl text-center"}>React-form-hook</div>
-      </Card>
-      <Card>
-        <div className={"text-xl text-center"}> @toolkit/Redux</div>
-      </Card>
-      <Card>
-        <div className={"text-xl text-center"}>Next.JS</div>
-      </Card>
-      <Card>
-        <div className={"text-xl text-center"}>Microfrontend</div>
-      </Card>
-      <Card>
-        <div className={"text-xl text-center"}>Angular</div>
-      </Card>
-      <Card>
-        <div className={"text-xl text-center"}> Material UI</div>
-      </Card>
-      <Card>
-        <div className={"text-xl text-center"}>Ecom</div>
+        <div className={"text-xl text-center"}>
+          <a
+            href="https://todo-gety9rw66-shashank-trivedis-projects-3a9462cb.vercel.app/"
+            target="_blank"
+          >
+            Todo- APP with Data Persistence
+          </a>
+        </div>
       </Card>
     </div>
   );
